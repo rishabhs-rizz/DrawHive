5de8a31336797f58cea1d1b8059caf01f35b6561
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-03-19 17:03:34 +0530

    added middleware and gated endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,30 +1,49 @@
 import express, { Request, Response } from "express";
 import { z } from "zod";
-import { connectDB } from "@repo/db/dbCall";
+import prisma from "@repo/db/prisma";
+import jwt from "jsonwebtoken";
+import JWT_SECRET from "@repo/common/config";
 
 const app = express();
 app.use(express.json());
 
 const SignUpBody = z.object({
   email: z.string().email(),
-  password: z.string().min(6).max(100),
   name: z.string().min(2).max(100),
+  password: z.string().min(6).max(100),
 });
 
-app.post("/signup", (req: Request, res: Response) => {
+app.post("/signup", async (req: Request, res: Response) => {
   try {
-    const body = SignUpBody.parse(req.body);
-    console.log(body);
-    res.send("Success");
+    const body = SignUpBody.safeParse(req.body);
+    if (body.success) {
+      await prisma.user.create({
+        data: {
+          email: body.data.email,
+          name: body.data.name,
+          password: body.data.password,
+        },
+      });
+    }
+    res.json({ message: "User created successfully" });
   } catch (error) {
     console.error(error);
     res.status(400).send("Invalid details provided");
   }
 });
 
-app.post("/signin", (req: Request, res: Response) => {
+app.post("/signin", async (req: Request, res: Response) => {
   const { email, password } = req.body;
   console.log(email, password);
+  const registeredUser = await prisma.user.findFirst({
+    where: { email: email },
+  });
+  if (registeredUser) {
+    const token = jwt.sign({ id: registeredUser.id }, JWT_SECRET as string);
+    res.json({ message: "User signed in successfully", token: token });
+  } else {
+    res.status(400).send("Invalid credentials provided");
+  }
 });
 
 app.listen(5000);
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,28 @@
+import JWT_SECRET from "@repo/common/config";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  id?: string;
+}
+
+export default function auth_middleWare(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): void {
+  const token = req.headers.authorization ?? "";
+
+  try {
+    if (token) {
+      const decode = jwt.verify(token, JWT_SECRET as string);
+      if (decode) {
+        req.id = (decode as { id: string }).id;
+        next();
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(401).send("Unauthorized");
+  }
+}
