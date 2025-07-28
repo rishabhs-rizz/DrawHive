58a13929bdd127dfc97db676eb6e986bf83b6004
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-28 17:33:43 +0530

    rectified handlers logic

--- apps/drawhive-frontend/Components/Handlers.tsx
+++ apps/drawhive-frontend/Components/Handlers.tsx
@@ -2,10 +2,7 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-export const handleCreateRoom = async (
-  roomName: string,
-  setRoomId: React.Dispatch<React.SetStateAction<string>>
-) => {
+export const handleCreateRoom = async (roomName: string) => {
   if (!roomName) return alert("Enter room name");
   console.log("Creating room with name:", roomName);
   const token = localStorage.getItem("token");
@@ -19,32 +16,34 @@ export const handleCreateRoom = async (
       },
     }
   );
-  const data = response.data;
-  const roomId = data.roomID;
-  setRoomId(roomId);
-  navigator.clipboard.writeText(roomId);
+  if (response) {
+    const link = response.data.link;
+    const roomID = response.data.roomID;
+    console.log("here's the link : " + link, "roomID " + roomID);
+    return { link, roomID };
+  }
 };
 
-export const handleCopy = (roomId: string) => {
-  navigator.clipboard.writeText(roomId);
-  alert("roomId copied to clipboard!");
+export const handleCopy = (link: string) => {
+  navigator.clipboard.writeText(link);
 };
 
-export const handleJoinRoom = (
+export const handleJoinRoom = async (
+  link: string,
   roomId: string,
   router: ReturnType<typeof useRouter>
 ) => {
-  if (!roomId) return alert("Enter room ID");
-  // console.log("Joining room with ID:", roomId);
-  // const token = localStorage.getItem("token");
-  // const response = await axios.get(`http://localhost:5000/room/${roomId}`, {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: `Bearer ${token}`,
-  //   },
-  // });
-  // const data = response.data;
-  // console.log("Room data:", data);
+  if (!link) return alert("Enter the link");
+  console.log("Joining room with the link:", link);
+  const token = localStorage.getItem("token");
+  const response = await axios.get(`http://localhost:5000/room/${link}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = response.data;
+  console.log("Room data:", data);
 
   router.push(`/canvas/${roomId}`);
 };
