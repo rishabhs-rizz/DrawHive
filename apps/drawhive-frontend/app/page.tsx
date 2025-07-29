b294d77eb0691e8001eee692bd0c289f39f88aa5
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-07-29 16:48:36 +0530

    changed initial route

--- apps/drawhive-frontend/app/page.tsx
+++ apps/drawhive-frontend/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
+import signUp from "./signup/page";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      drawhive landing page
-    </div>
-  );
+  const router = useRouter();
+  router.push("/signup");
 }
